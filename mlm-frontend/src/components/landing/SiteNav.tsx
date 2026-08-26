f498e34616dc807862f59b848dddc05e199ba6a2
bestwork-server <server@mahmutgazihanarslan.com.tr>
2026-08-26 04:02:37 +0300

    fix: menü %80 genislik

--- mlm-frontend/src/components/landing/SiteNav.tsx
+++ mlm-frontend/src/components/landing/SiteNav.tsx
@@ -114,7 +114,7 @@ export default function SiteNav() {
           top: 0,
           left: "50%",
           transform: "translateX(-50%)",
-          width: "90%",
+          width: "80%",
           height: 95,
           bgcolor: "background.default",
           boxShadow: "none",
